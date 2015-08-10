0199f4b3e74af026c5c491370ba830a2d3c0dc53
Erik Yuzwa <erik@erikyuzwa.com>
2015-08-10 00:34:43 -0600

    updated to initialize core sub systems

--- source/app/AppCore.cpp
+++ source/app/AppCore.cpp
@@ -1,99 +1,163 @@
 
 #include "AppCore.h"
 
-AppCore::AppCore() {
-    
-    mainWindow = NULL;
-    mainRenderer = NULL;
-    w = 800;
-    h = 600;
-    
-}
+namespace core {
 
-AppCore::~AppCore() {
+	AppCore::AppCore() {
     
-    
-}
+		mainWindow = NULL;
+		mainRenderer = NULL;
+		w = 800;
+		h = 600;
 
-int AppCore::startup() {
+		mainAudio = NULL;
+		mainFont = NULL;
     
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        printf("error initializing SDL : %s\n", SDL_GetError());
-        return -1;
-    }
-    
-    mainWindow = SDL_CreateWindow( "SDL2 - helloworld - press ESC to quit",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        w, h,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
-    if (mainWindow == NULL) {
-        printf("error initializing SDL_Window: %s\n", SDL_GetError());
-        return -2;
-    }
-    
-    mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (mainRenderer == NULL) {
-        printf("error initializing SDL_Renderer: %s\n", SDL_GetError());
-        return -3;
-    }
-    
-    //TODO - in your own extended object, handle initialization
-    if (startupApp() < 0){
-        printf("error initializing app\n");
-        return -4;
-    }
-    
-    return 0;
-}
+	}
 
-int AppCore::update() {
+	AppCore::~AppCore() {
     
-    SDL_Event e;
+		SDL_assert(mainRenderer == NULL);
+		SDL_assert(mainWindow == NULL);
+	}
+
+	int AppCore::startup() {
     
-    bool done = false;
+		if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+			printf("error initializing SDL : %s\n", SDL_GetError());
+			return -1;
+		}
     
-    //While application is running
-    while( !done ) {
-        //Handle events on queue
-        while( SDL_PollEvent( &e ) != 0 ) {
-            //User requests quit by closing the window
-            if ( e.type == SDL_QUIT ) {
-                done = true;
-            } else if( e.type == SDL_KEYDOWN ) {
-                //also quit on the ESC key
-                if( e.key.keysym.sym == SDLK_ESCAPE ) {
-                    done = true;
-                }
-            }
-        }
+		mainWindow = SDL_CreateWindow( "this could be YOUR title! - press ESC to quit",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			w, h,
+			SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
+		if (mainWindow == NULL) {
+			printf("error initializing SDL_Window: %s\n", SDL_GetError());
+			return -2;
+		}
+    
+		mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		if (mainRenderer == NULL) {
+			printf("error initializing SDL_Renderer: %s\n", SDL_GetError());
+			return -3;
+		}
+
+		// make sure we start up some of our subsystems
+		Uint32 imageFlags = IMG_INIT_PNG;
+		if(!(IMG_Init(imageFlags) & imageFlags)){
+			printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+			return -4;
+		}
+
+		mainFont = new SysFont();
+		if(mainFont->startup() < 0){
+			printf( "SysFont could not initialize - no font support for this session");
+			return -5;
+		}
+
+ 	    // attempt to load and initialize our SDL_Mixer lightweight wrapper. Change
+		// this if you feel different, but if we encounter an error initializing
+		// audio, we should still be able to play a game (just with no audio of course)
+		mainAudio = new SysAudio();
+		if (mainAudio->startup() < 0) {
+			printf( "SysAudio could not initialize - no sound or music for this session");
+		}
+    
+		// TODO - in your own extended object, handle initialization
+		if (startupApp() < 0) {
+			printf("error initializing app\n");
+			return -10;
+		}
+    
+		return 0;
+	}
+
+	int AppCore::update() {
+    
+		SDL_Event e;
+    
+		bool done = false;
+
+		int frameCount = 0;
+
+		float avgFps = 0.0f;
+
+		frameTimer.run();
+    
+		// While application is running
+		while( !done ) {
+			// Handle events on queue
+			while( SDL_PollEvent( &e ) != 0 ) {
+				// User requests quit by closing the window
+				if ( e.type == SDL_QUIT ) {
+					done = true;
+				} else if( e.type == SDL_KEYDOWN ) {
+					// also quit on the ESC key
+					if( e.key.keysym.sym == SDLK_ESCAPE ) {
+						done = true;
+					}
+				}
+			}
+
+			// update our fps calculation
+			avgFps = frameCount / ( frameTimer.getTicks() / 1000.0f);
+			// an upper bound guard
+			if (avgFps > 2000) {
+				avgFps = 0;
+			}
         
-        //clear screen to blue
-        SDL_SetRenderDrawColor( mainRenderer, 0x00, 0x00, 0xFF, 0xFF );
-        SDL_RenderClear( mainRenderer );
+			// clear screen to blue
+			SDL_SetRenderDrawColor( mainRenderer, 0x00, 0x00, 0xFF, 0xFF );
+			SDL_RenderClear( mainRenderer );
         
-        //TODO - in your own extended object, handle update and rendering
-        updateApp();
+			// TODO - in your own extended object, handle update and rendering
+			updateApp(frameTimer.getTicks());
         
-        //render everything to the screen
-        SDL_RenderPresent( mainRenderer );
+			// render everything to the screen
+			SDL_RenderPresent( mainRenderer );
     
-        //carryover of SDL1.2 hack for making sure app doesn't starve OS
-        SDL_Delay(100);
-    }
-    
-    return 0;
-}
+			// carryover of SDL1.2 hack for making sure app doesn't starve OS
+			SDL_Delay(100);
 
-void AppCore::shutdown() {
+			// increase our frame counter
+			++frameCount;
+		}
     
-    //TODO in your own extended object, handle shutdown and cleanup
-    shutdownApp();
+		return 0;
+	}
+
+	void AppCore::shutdown() {
     
-    SDL_DestroyRenderer(mainRenderer);
-    SDL_DestroyWindow(mainWindow);
-    mainRenderer = NULL;
-    mainWindow = NULL;
+		// TODO in your own extended object, handle shutdown and cleanup
+		shutdownApp();
+
+		// cleanup our audio subsystem
+		if(mainAudio != NULL){
+			mainAudio->shutdown();
+			delete mainAudio;
+			mainAudio = NULL;
+		}
+
+		// cleanup our font subsystem
+		if (mainFont != NULL) {
+			mainFont->shutdown();
+			delete mainFont;
+			mainFont = NULL;
+		}
     
-    SDL_Quit();
+	    // cleanup and destroy our SDL_Renderer and SDL_Window objects
+		SDL_DestroyRenderer(mainRenderer);
+		SDL_DestroyWindow(mainWindow);
+		mainRenderer = NULL;
+		mainWindow = NULL;
+
+        // cleanup the SDL_Image subsystem
+		IMG_Quit();
+		
+		// cleanup any remaining SDL core objects
+		SDL_Quit();
     
+	}
+
 }
--- source/app/AppCore.h
+++ source/app/AppCore.h
@@ -1,7 +1,10 @@
 
 #pragma once
 
-#include "stdafx.h"
+#include "SysTimer.h"
+#include "SysAudio.h"
+#include "SysFont.h"
+#include "SysSprite.h"
 
 /**
 * AppCore
@@ -10,48 +13,62 @@
 * your application away from SDL2. The benefit here is being able to update SDL2 components
 * without needing to rework your entire application.
 */
-class AppCore {
+namespace core {
+
+	class AppCore {
     
-protected:
-    /** display width */
-    int           w;
+	protected:
+		/** display width */
+		int           w;
     
-    /** display height */
-    int           h;
+		/** display height */
+		int           h;
     
-    /** SDL2 window pointer */
-    SDL_Window*   mainWindow;
+		/** SDL2 window pointer */
+		SDL_Window*   mainWindow;
     
-    /** SDL2 renderer pointer */
-    SDL_Renderer* mainRenderer;
+		/** SDL2 renderer pointer */
+		SDL_Renderer* mainRenderer;
+
+		/** a system timer to track fps */
+		SysTimer frameTimer;
+
+		/** a lightweight core object to wrap SDL_Mixer functionality */
+		SysAudio* mainAudio;
+
+		/** a lightweight core object to wrap SDL_TTF functionality */
+		SysFont* mainFont;
     
-public:
-    /**
-     * Constructor
-     */
-    AppCore();
+	public:
+		/**
+		 * Constructor
+		 */
+		AppCore();
     
-    /**
-     * Destructor
-     */
-    ~AppCore();
+		/**
+		 * Destructor
+		 */
+		~AppCore();
     
-public:
-    /** startup and initialize our SDL2 base */
-    int startup();
+	public:
+		/** startup and initialize our SDL2 base */
+		int startup();
     
-    /** run the main event loop which also draws */
-    int update();
+		/** run the main event loop which also draws */
+		int update();
     
-    /** shut everything down and cleanup */
-    void shutdown();
+		/** shut everything down and cleanup */
+		void shutdown();
     
-    /** exposed function for handling your own startup and object creation */
-    virtual int startupApp(){ return 0; }
+		/** exposed function for handling your own startup and object creation */
+		virtual int startupApp(){ return 0; }
     
-    /** exposed function for handling your own update and render loop */
-    virtual void updateApp(){};
+		/** exposed function for handling your own update and render loop */
+		virtual void updateApp(Uint32 delta){};
     
-    /** exposed function for handling your own shutdown and cleanup */
-    virtual void shutdownApp(){};
-};
+		/** exposed function for handling your own shutdown and cleanup */
+		virtual void shutdownApp(){};
+	};
+
+}
+
